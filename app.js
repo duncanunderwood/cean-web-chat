1d439213d7c5dfacce865b179d08c15204673c45
duncanunderwood <duncanunderwood@me.com>
2016-02-01 23:07:35 +1100

    third commit

---
+++ app.js
@@ -0,0 +1,42 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var couchbase = require("couchbase");
+var path = require("path");
+var config = require("./config");
+var app = express();
+
+var server = require("http").Server(app);
+var io = require("socket.io").listen(server);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+module.exports.bucket = (new couchbase.Cluster(config.couchbase.server)).openBucket(config.couchbase.bucket);
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/scripts", express.static(__dirname + "/node_modules/"));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
+var routes = require("./routes/routes.js")(app);
+var ChatModel = require("./models/chatmodel.js");
+
+io.on("connection", function(socket){
+    socket.on("chat_message", function(msg){
+        ChatModel.create({message: msg}, function(error, result) {
+            if(error) {
+                console.log(JSON.stringify(error));
+            }
+            io.emit("chat_message", msg);
+        });
+    });
+});
+
+server.listen(3000, function () {
+    console.log("Listening on port %s...", server.address().port);
+});
